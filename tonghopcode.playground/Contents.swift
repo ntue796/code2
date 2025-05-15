169e6311cfcd7582b1c250f95ffd6c0dd8271aaa
tue <>
2025-05-15 22:37:47 +0700

    add string code

--- tonghopcode.playground/Contents.swift
+++ tonghopcode.playground/Contents.swift
@@ -187,3 +187,26 @@ var c4 = -13.4 / 2
 var bt = (10 + 2) / 5 * (4 - 7)
 
 
+var greeting: String = "Hello, playground"
+
+var generation = 5
+var carBrand = "Honda"
+var carModel = "Civic"
+var carName = carBrand + " " + carModel
+var carName2 = "\(carBrand) \(carModel) \(generation)"
+carName.append(contentsOf: " 5")
+
+
+var myName = "ngoc tue"
+myName = myName.capitalized
+
+var capsAll = "HAHAHA"
+var lowercaseAll = capsAll.lowercased()
+
+var message = "choi game ngu qua"
+message.replacingOccurrences(of: "ngu", with: "ga")
+
+if message.contains("ngu") {
+    message = "******"
+}
+
