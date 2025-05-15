5c5c419b52dd40b6cd1807dbf1167513ce127232
tue <>
2025-05-15 22:30:21 +0700

    add array code

--- tonghopcode.playground/Contents.swift
+++ tonghopcode.playground/Contents.swift
@@ -1,3 +1,18 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var studentName1 = "tue"
+var studentName2 = "truong"
+var studentName3 = "quyen"
+var studentName4 = "thuy"
+
+var studentNames: [String] = ["tue", "truong", "quyen", "thuy"]
+print(studentNames.count)
+studentNames.append("halo")
+print(studentNames.count)
+
+studentNames.remove(at: 1)
+
+print(studentNames.count)
+
+var carNames = [String]()
+print(carNames.count)
